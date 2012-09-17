22f30ae28fa857ec4e4ee520c74cd910a8c508f0
Brett Sanders <brettrsanders@gmail.com>
2012-09-16 18:24:06 -0700

    Add quote validations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,5 @@
 class Quote < ActiveRecord::Base
   attr_accessible :page, :text
+  validates :page, presence: true
+  validates :text, presence: true
 end
--- db/migrate/20120917011924_create_quotes.rb
+++ db/migrate/20120917011924_create_quotes.rb
@@ -1,8 +1,8 @@
 class CreateQuotes < ActiveRecord::Migration
   def change
     create_table :quotes do |t|
-      t.text :text
-      t.integer :page
+      t.text    :text,  null: false
+      t.integer :page,  null: false
 
       t.timestamps
     end
